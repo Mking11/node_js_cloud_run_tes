9734a365ff26536e29b4966c156952ab82f11c8c
Michael Mekonnen <michaelboymek@gmail.com>
2021-09-03 08:23:04 +0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT ||8080;
+const port = process.env.TESt_PORT ||8081;
 app.get("/test/", async (req,res) =>{
     return res.status(200).send("server_hit")
 })
